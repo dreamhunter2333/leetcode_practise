a0140df098e2f098114472c16a8ce13da1d50302
jinmu333 <jinmu333@qq.com>
2019-09-22 15:33:40 +0800

    pep8格式化

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ if __name__ == '__main__':
         short_list.append(min([people[col][row] for row in range(10)]))
     print("高子： " + str(high_list))
     print("矮子： " + str(short_list))
-    print("「高子里的矮子」: " + str(min(high_list)) + "\n「矮子里的高子」: " + str(max(short_list)))
+    print("「高子里的矮子」: " + str(min(high_list)) +
+          "\n「矮子里的高子」: " + str(max(short_list)))
     # 为什么「高子里的矮子」>「矮子里的高子」
     high_position = []
     short_position = []
@@ -51,6 +52,8 @@ if __name__ == '__main__':
     #     print(people[high_position[0]][high_position[1]] >= person)
 
     # 高子里的矮子 >= 高子里的矮子 所在行 和 矮子里的高子所在列 的人
-    print(str(people[high_position[0]][high_position[1]]) + '>=' + str(people[high_position[0]][short_position[1]]))
+    print(str(people[high_position[0]][high_position[1]]) +
+          '>=' + str(people[high_position[0]][short_position[1]]))
     # 交叉处的人 >= 矮子里的高子
-    print(str(people[high_position[0]][short_position[1]]) + '>=' + str(people[short_position[0]][short_position[1]]))
+    print(str(people[high_position[0]][short_position[1]]) +
+          '>=' + str(people[short_position[0]][short_position[1]]))
