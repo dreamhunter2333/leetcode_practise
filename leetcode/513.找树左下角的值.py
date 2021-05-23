7d8a4be72482d019fd620ac8005229995576e17d
jinmu333 <jinmu333@qq.com>
2021-05-23 17:22:14 +0800

    feat: add [513] 找树左下角的值

---
+++ leetcode/513.找树左下角的值.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode.cn id=513 lang=python3
+#
+# [513] 找树左下角的值
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import List
+
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findBottomLeftValue(self, root: TreeNode) -> int:
+        if not root:
+            raise
+        res = None
+        node_list = [root]
+        while (len(node_list)):
+            res = node_list[0].val
+            new_list = []
+            for node in node_list:
+                if node.left:
+                    new_list.append(node.left)
+                if node.right:
+                    new_list.append(node.right)
+            node_list = new_list
+        return res
+
+# @lc code=end
