27f8a0f3a5af9cba8750d975a4a6b5c45f99a6b6
jinmu333 <jinmu333@qq.com>
2021-06-05 23:56:18 +0800

    feat: add [518] 零钱兑换 II

---
+++ leetcode/518.零钱兑换-ii.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=518 lang=python3
+#
+# [518] 零钱兑换 II
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount+1)
+        dp[0] = 1
+        for coin in coins:
+            for j in range(coin, amount+1):
+                dp[j] += dp[j - coin]
+        return dp[amount]
+
+# @lc code=end
