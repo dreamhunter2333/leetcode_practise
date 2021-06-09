670388f068e923794b2e70f1a4a12821e30968b6
jinmu333 <jinmu333@qq.com>
2021-06-09 22:36:04 +0800

    feat: add [879] 盈利计划

---
+++ leetcode/879.盈利计划.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=879 lang=python3
+#
+# [879] 盈利计划
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        MOD = 10**9 + 7
+        dp = [[0] * (minProfit + 1) for _ in range(n + 1)]
+        for i in range(0, n + 1):
+            dp[i][0] = 1
+        for earn, members in zip(profit, group):
+            for j in range(n, members - 1, -1):
+                for k in range(minProfit, -1, -1):
+                    dp[j][k] = (dp[j][k] + dp[j - members][max(0, k - earn)]) % MOD
+        return dp[n][minProfit]
+
+
+# @lc code=end
