dcb1968e3798c00af4a595239584741082925b9b
jinmu333 <jinmu333@qq.com>
2021-06-13 23:51:57 +0800

    feat: add [155] 最小栈

---
+++ leetcode/155.最小栈.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode.cn id=155 lang=python3
+#
+# [155] 最小栈
+#
+
+# @lc code=start
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = [float('inf')]
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.min_stack.append(min(x, self.min_stack[-1]))
+
+    def pop(self) -> None:
+        res = self.stack.pop()
+        self.min_stack.pop()
+        return res
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
