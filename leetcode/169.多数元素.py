33f7f93ac536f8f337b2b1b83bd433b19d6cd43a
jinmu333 <jinmu333@qq.com>
2021-06-14 21:41:37 +0800

    feat: add [169] 多数元素

---
+++ leetcode/169.多数元素.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=169 lang=python3
+#
+# [169] 多数元素
+#
+
+# @lc code=start
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums) // 2];
+
+# @lc code=end
