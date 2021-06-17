2f0e6287c71c9f87da9a19025c95871e7218c537
jinmu333 <jinmu333@qq.com>
2021-06-17 22:30:48 +0800

    feat: add [101] 对称二叉树

---
+++ leetcode/101.对称二叉树.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=101 lang=python3
+#
+# [101] 对称二叉树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def dfs(node1, node2):
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2:
+                return False
+            return node1.val == node2.val and dfs(
+                node1.left, node2.right
+            ) and dfs(node1.right, node2.left)
+        return dfs(root.left, root.right)
+
+# @lc code=end
