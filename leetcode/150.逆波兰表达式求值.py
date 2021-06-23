61153a1fee8d5e7f647d15a2c9655410630e12eb
jinmu333 <jinmu333@qq.com>
2021-06-23 21:38:23 +0800

    feat: add [150] 逆波兰表达式求值

---
+++ leetcode/150.逆波兰表达式求值.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode.cn id=150 lang=python3
+#
+# [150] 逆波兰表达式求值
+#
+
+# @lc code=start
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        res = []
+        for num in tokens:
+            if num == '+':
+                res.append(res.pop() + res.pop())
+            elif num == '-':
+                num2 = res.pop()
+                num1 = res.pop()
+                res.append(num1 - num2)
+            elif num == '/':
+                num2 = res.pop()
+                num1 = res.pop()
+                if num1 * num2 < 0:
+                    res.append(-(num1 // -num2))
+                else:
+                    res.append(num1 // num2)
+            elif num == '*':
+                num2 = res.pop()
+                num1 = res.pop()
+                res.append(num1 * num2)
+            else:
+                res.append(int(num))
+
+        return res[0]
+
+# @lc code=end
