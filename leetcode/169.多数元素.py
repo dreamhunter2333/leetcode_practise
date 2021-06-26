fc4c2110ee273c6a4199917d35f049fb1c21ad89
jinmu333 <jinmu333@qq.com>
2021-06-26 19:08:24 +0800

    feat: update [169] 多数元素

--- leetcode/169.多数元素.py
+++ leetcode/169.多数元素.py
@@ -7,7 +7,18 @@
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums) // 2];
+        # nums.sort()
+        # return nums[len(nums) // 2]
+        # 摩尔投票
+        count = 0
+        for num in nums:
+            if count == 0:
+                res = num
+            if num == res:
+                count += 1
+            else:
+                count -= 1
+
+        return res
 
 # @lc code=end
