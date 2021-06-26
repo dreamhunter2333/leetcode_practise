46a2fafacd81eda249dc1cabb8c716d39cf95396
jinmu333 <jinmu333@qq.com>
2021-06-26 17:07:20 +0800

    feat: add [283] 移动零

---
+++ leetcode/283.移动零.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=283 lang=python3
+#
+# [283] 移动零
+#
+
+# @lc code=start
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        left = right = 0
+        while right < n:
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+
+# @lc code=end
