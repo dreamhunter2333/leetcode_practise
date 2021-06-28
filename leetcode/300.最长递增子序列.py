29bbbd41c5811bfce6c151aab67158ca8c36a091
jinmu333 <jinmu333@qq.com>
2021-06-28 18:20:48 +0800

    feat: add [300] 最长递增子序列

---
+++ leetcode/300.最长递增子序列.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=300 lang=python3
+#
+# [300] 最长递增子序列
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        n = len(nums)
+        dp = [1] * n
+
+        for i in range(n):
+            for j in range(i):
+                if nums[i] <= nums[j]:
+                    continue
+                dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+
+
+print(Solution().lengthOfLIS([0, 1, 0, 3, 2, 3]))
+# @lc code=end
