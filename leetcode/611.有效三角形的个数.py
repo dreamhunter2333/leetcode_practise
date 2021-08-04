364cc9007186a88e9e08965026342afef5025f0a
jinmu333 <dreamhunter2333@gmail.com>
2021-08-04 19:21:36 +0800

    feat: add [611] 有效三角形的个数

---
+++ leetcode/611.有效三角形的个数.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=611 lang=python3
+#
+# [611] 有效三角形的个数
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        ans = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                left, right, k = j + 1, n - 1, j
+                while left <= right:
+                    mid = (left + right) // 2
+                    if nums[mid] < nums[i] + nums[j]:
+                        k = mid
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+                ans += k - j
+        return ans
+
+
+# @lc code=end
