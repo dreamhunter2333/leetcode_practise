6694ce2f9ae7f2d9a8e417c9a78b5c8cf96a8388
jinmu333 <dreamhunter2333@gmail.com>
2021-10-04 15:40:53 +0800

    feat: update [482] 密钥格式化

--- leetcode/482.密钥格式化.py
+++ leetcode/482.密钥格式化.py
@@ -9,16 +9,14 @@ class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
         res = []
         count = 0
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] == '-':
+        for c in s[::-1]:
+            if c == '-':
                 continue
-            res.append(s[i].upper())
-            count += 1
             if count == k:
                 count = 0
                 res.append('-')
-        if len(res) % (k + 1) == 0 and res:
-            res.pop()
+            res.append(c.upper())
+            count += 1
         return ''.join(reversed(res))
 
 # @lc code=end
