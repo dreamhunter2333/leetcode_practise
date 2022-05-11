04cfc1e44ddd562fe311e8057eb72626ca712298
jinmu333 <dreamhunter2333@gmail.com>
2022-05-11 23:32:35 +0800

    feat: add [449] 序列化和反序列化二叉搜索树

---
+++ leetcode/449.序列化和反序列化二叉搜索树.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode.cn id=449 lang=python3
+#
+# [449] 序列化和反序列化二叉搜索树
+#
+
+# @lc code=start
+# # Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Codec:
+    def serialize(self, root: TreeNode) -> str:
+        arr = []
+
+        def postOrder(root: TreeNode) -> None:
+            if root is None:
+                return
+            postOrder(root.left)
+            postOrder(root.right)
+            arr.append(root.val)
+        postOrder(root)
+        return ' '.join(map(str, arr))
+
+    def deserialize(self, data: str) -> TreeNode:
+        arr = list(map(int, data.split()))
+
+        def construct(lower: int, upper: int) -> TreeNode:
+            if arr == [] or arr[-1] < lower or arr[-1] > upper:
+                return None
+            val = arr.pop()
+            root = TreeNode(val)
+            root.right = construct(val, upper)
+            root.left = construct(lower, val)
+            return root
+        return construct(float("-inf"), float("inf"))
+
+
+# Your Codec object will be instantiated and called as such:
+# node = TreeNode(2)
+# node.left = TreeNode(1)
+# node.right = TreeNode(3)
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(None)
+# ans = deser.deserialize(tree)
+# print()
+# @lc code=end
