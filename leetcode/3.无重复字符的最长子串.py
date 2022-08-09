eb2bec4e0a7134471bd7c6d11b36bce80c979050
dreamhunter <dreamhunter2333@gmail.com>
2022-08-09 22:43:27 +0800

    feat: update [3] 无重复字符的最长子串

--- leetcode/3.无重复字符的最长子串.py
+++ leetcode/3.无重复字符的最长子串.py
@@ -3,41 +3,6 @@
 #
 # [3] 无重复字符的最长子串
 #
-# https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
-#
-# algorithms
-# Medium (32.02%)
-# Likes:    2729
-# Dislikes: 0
-# Total Accepted:    275K
-# Total Submissions: 858.6K
-# Testcase Example:  '"abcabcbb"'
-#
-# 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
-# 
-# 示例 1:
-# 
-# 输入: "abcabcbb"
-# 输出: 3 
-# 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
-# 
-# 
-# 示例 2:
-# 
-# 输入: "bbbbb"
-# 输出: 1
-# 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
-# 
-# 
-# 示例 3:
-# 
-# 输入: "pwwkew"
-# 输出: 3
-# 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-# 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
-# 
-# 
-#
 
 # @lc code=start
 class Solution:
@@ -45,22 +10,20 @@ class Solution:
         if not s:
             return 0
         left = 0
-        on = set()
-        on_len = 0
+        cache_set = set()
+        cache_set_len = 0
         max_len = 0
-        for index in range(len(s)):
-            on_len += 1
-            while s[index] in on:
+        for char in s:
+            while char in cache_set:
                 # 移出左侧的字符直到没有重复
-                on.remove(s[left])
+                cache_set.remove(s[left])
                 left += 1
-                on_len -= 1
-            if on_len > max_len:max_len = on_len
-            on.add(s[index])
+                cache_set_len -= 1
+            cache_set.add(char)
+            cache_set_len += 1
+            max_len = max(max_len, cache_set_len)
         return max_len
 
 
-if __name__ == "__main__":
-    print(Solution().lengthOfLongestSubstring('abccccccddddacc'))
 # @lc code=end
-
+print(Solution().lengthOfLongestSubstring('abccccccddddacc'))
